581ada36b9a877dd82536f678057b143c489e77b
sajithprasanga <54270163+sajithprasanga@users.noreply.github.com>
2020-05-24 14:20:25 +0530

    Q2

--- Q2.scala
+++ Q2.scala
@@ -4,6 +4,10 @@ object Q2{
 	{
 		print("Enter radius: ");
 		val r = scala.io.StdIn.readDouble();
+		volume(r);
+	}
+	
+	def volume(r:Double):Unit={
 		val pi=3.418;
 		val vol = (4/3)*pi*pow(r,3);
 		print(f"Volume: ${vol}%.3f");
