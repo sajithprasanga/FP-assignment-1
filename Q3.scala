d845aad51cbd7cc8d77f05fd1c9aa1ba7a00e29c
sajithprasanga <54270163+sajithprasanga@users.noreply.github.com>
2020-05-24 14:24:13 +0530

    Q3

--- Q3.scala
+++ Q3.scala
@@ -1,8 +1,11 @@
 object Q3{
 	def main(args:Array[String]){
+		cost(60);	
+	}
+
+	def cost(con:Int):Unit={
 		var covpri = 24.95;
 		var dis = 24.95-(24.95*0.4);
-		var con = 60;
 		if(con>50){
 			var bala = (covpri*con)-(covpri*con*0.4)+(50*3)+((con-50)*0.75);
 			println(f"Cost of copies: ${bala}%.2f");
@@ -11,6 +14,5 @@ object Q3{
 			var bala = (covpri*con)-(covpri*con*0.4)+(con*3.00);
 			println(f"Cost of copies: ${bala}%.2f");
 		}
-		
 	}
 }
